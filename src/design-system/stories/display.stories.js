6b19fc14626d7d56c5f07cb50e233799ba9933c7
Nathaniel Kessler <hey@nathanielkessler.com>
2020-06-18 10:18:19 -0400

    adds ds figure card vertical

--- src/design-system/stories/display.stories.js
+++ src/design-system/stories/display.stories.js
@@ -2,8 +2,10 @@ import React from 'react'
 import { 
   ImageBox as ImageBoxComponent,
   FigureCard as FigureCardComponent,
-  LinkButton,
-} from './../components';
+  FigureCardVertical as FigureCardVerticalComponent,
+  LinkButton
+} from './../components'
+
 
 export default {
   title: 'Display',
@@ -24,5 +26,16 @@ export const FigureCard = () => {
       <p className="copy">Chicken is a type of domesticated fowl, a subspecies of the red junglefowl. It is one of the most common and widespread domestic animals, with a total population of more than 19 billion as of 2011.[1] Humans commonly keep chickens as a source of food (consuming both their meat and eggs) and, more rarely, as pets.</p>
       <LinkButton rightIcon="arrow-right" className="mt-4">More</LinkButton>
     </FigureCardComponent>
+  )
+}
+
+export const Test = () => {
+  return (
+    <FigureCardVerticalComponent 
+      title="Brown Stew Chicken"
+      src="https://www.themealdb.com/images/category/beef.png"
+    >
+      <p className="copy text-center">Lorem ipsom</p>
+    </FigureCardVerticalComponent>
   )
 }
