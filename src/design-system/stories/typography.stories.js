882960030f783eba52fc7b186148e07508465c87
Nathaniel Kessler <hey@nathanielkessler.com>
2020-06-17 22:44:51 -0400

    adds typography tokens and stories

---
+++ src/design-system/stories/typography.stories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default {
+  title: 'Typography',
+}
+
+const TextCard = ({ className, children }) => (
+  <p className={`${className}`}>{children}</p>
+)
+
+export const Typography = () => (
+  <div>
+    <TextCard className="heading-1">heading-1</TextCard>
+    <TextCard className="heading-2">heading-2</TextCard>
+    <TextCard className="copy">copy</TextCard>
+    <TextCard className="copy-bold">copy-bold</TextCard>
+    <TextCard className="emphasize">emphasize</TextCard>
+  </div>
+)
