1b09cc7c2a9b2ba346e0fb1f155874a79b580f38
Nathaniel Kessler <hey@nathanielkessler.com>
2020-06-19 10:56:58 -0400

    adds focus state to modal

--- src/design-system/components/modal.js
+++ src/design-system/components/modal.js
@@ -1,12 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { createRef } from 'react'
 import { createPortal } from 'react-dom'
 import { Icon } from './icon'
+import { useKeyboardEvents } from './../hooks/use-keyboard-events'
 
-const escapeEvent = (callBack) => (e) => {
-  if (e.keyCode === 27) {
-    callBack();
-  }
-}
 
 export const Modal =  ({
   onClose = () => {},
@@ -14,16 +10,33 @@ export const Modal =  ({
   className,
 }) => {
 
+  const modalRef = createRef()
+  const handleTabbing = (e) => {
+    let somethingActive = false;
+    const activeElement = document.activeElement
+    const tabbables = modalRef.current.querySelectorAll(
+      'a[href], button, textarea, input[type="text"], input[type="radio"], input[type="checkbox"], select'
+    )
+    
+    tabbables.forEach((node) => {
+      if (activeElement.isSameNode(node)) {
+        somethingActive = true
+      }
+    })
 
- useEffect(() => {
-  const escapeClicked = escapeEvent(onClose); 
-  document.addEventListener('keydown', escapeClicked);
-  return () => { document.removeEventListener('keydown', escapeClicked); }
- }, [])
+    if (!somethingActive) {
+      tabbables[0].focus()
+      return e.preventDefault()
+    }
+  }
+
+  useKeyboardEvents(9, handleTabbing)
+  useKeyboardEvents(27, onClose)
+
 
   return createPortal(
     <div className="bg-black bg-opacity-75 fixed left-0 right-0 bottom-0 top-0 p-10 overflow-y-scroll" role="dialog" aria-modal="true">
-      <div className={`box-border bg-white rounded-md p-5 pb-10 relative ${className}`}>
+      <div ref={modalRef} className={`box-border bg-white rounded-md p-5 pb-10 relative ${className}`}>
         <button onClick={onClose} className="absolute right-0 top-0 mr-3 mt-3" aria-label="close">
           <Icon name="close"/>
         </button>
