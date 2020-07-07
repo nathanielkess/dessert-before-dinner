00e71006f0084ab35bacd8101acbc9900948d244
Nathaniel Kessler <hey@nathanielkessler.com>
2020-07-06 20:08:21 -0400

    memoize pill button to prevent unnecessary re-renders

--- src/app/ui/components/categories-list.js
+++ src/app/ui/components/categories-list.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { PillButton } from '../../../design-system/components'
 
 export const CategoriesList = ({
@@ -6,21 +6,22 @@ export const CategoriesList = ({
   selectedCategory,
   categories,
 }) => {
-  const handleClick = (categoryName) => () => onSelected(categoryName);
+  const handleClick = useCallback((categoryName) => { onSelected(categoryName) }, [categories]);
   return (
     <nav className="flex flex-row flex-wrap justify-center">
-      {
-        categories.map(({id, name}) => (
-          <PillButton 
-            onClick={handleClick(name)}
-            key={id}
-            on={selectedCategory === name}
-            className="mx-2 my-2"
-          >
-            {name}
-          </PillButton>
-        ))
-      }
+        {
+          categories.map(({id, name}) => (
+            <PillButton 
+              onClick={handleClick}
+              onClickValue={name}
+              key={id}
+              on={selectedCategory === name}
+              className="mx-2 my-2"
+            >
+              {name}
+            </PillButton>
+          ))
+        }
     </nav>
   )
 }
